12ac4d80720e5b03ad915d04a2484a0a92a45f7b
Ewan Bauld <ewan.bauld@gmail.com>
2018-12-06 15:37:52 +0000

    Add Landing Page component (Pass)

---
+++ src/components/LandingPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class LandingPage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>NASA Media Viewer</h1>                
+            </div>
+        )
+    }
+}
+export default LandingPage;
