7a8bbe25f4100db8361f9b55663565620b15316a
Ewan Bauld <ewan.bauld@gmail.com>
2018-12-06 16:23:29 +0000

    Add filter tests (fail)

---
+++ src/actions/filter.js
@@ -0,0 +1,26 @@
+import { setTextFilter, setMediaByImage, setMediaByAudio } from '../actions/filter';
+import { SET_TEXT_FILTER, SET_MEDIA_BY_AUDIO, SET_MEDIA_BY_IMAGE} from '../actions/types';
+
+describe('Filter action object', () => {
+    it('should set up the text filter object', () => {
+        const action = setTextFilter('moon');
+        expect(action).toEqual({
+            type: SET_TEXT_FILTER,
+            text: 'moon'
+        })
+    });
+    it('should set the set by audio object', () => {
+        const action = setMediaByAudio('audio')
+        expect(action).toEqual({
+            type: SET_MEDIA_BY_AUDIO,
+            audio: 'audio'
+        });
+    });
+    it('should set the set by immage object', () => {
+        const action = setMediaByImage('image');
+        expect(action).toEqual({
+            type: SET_MEDIA_BY_IMAGE,
+            image: 'image'
+        })
+    });
+});
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,1 +1,4 @@
 export const SET_MEDIA = 'SET_MEDIA';
+export const SET_TEXT_FILTER = 'SET_TEXT_FILTER';
+export const SET_MEDIA_BY_IMAGE = 'SET_MEDIA_BY_IMAGE';
+export const SET_MEDIA_BY_AUDIO = 'SET_MEDIA_BY_AUDIO';
