a777b5a98ff61cb7cd5a44bcd0f1ee7c535f8d6a
Ewan Bauld <ewan.bauld@gmail.com>
2018-12-06 17:10:37 +0000

    Add redux and react router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import AppRouter from './routers/AppRouter';
+import getAppStore from './store/store';
+import { setMedia } from './actions/media';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = getAppStore();
+const NasaApp = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+)
+
+ReactDOM.render(NasaApp, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,7 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import mediaReducer from '../reducers/mediaReducer';
+import filtersReducer from '../reducers/filtersReducer';
 
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -7,6 +9,8 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export default () => {
     const store = createStore(
         combineReducers({
+            media: mediaReducer,
+            filters: filtersReducer
         }),
         composeEnhancers(applyMiddleware(thunk))
     );
