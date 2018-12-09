68b21ef4db378339e0bd6d4a21b95dbff0509a84
Ewan Bauld <ewan.bauld@gmail.com>
2018-12-09 16:28:49 +0000

    Add basic AssetItem component (pass)

---
+++ src/components/AssetItem.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class AssetItem extends Component {
+  render() {
+    return (
+      <div>
+          <h1>Asset Item</h1>
+      </div>
+    )
+  }
+}
+export default AssetItem;
